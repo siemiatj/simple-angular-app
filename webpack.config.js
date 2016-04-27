df1e79ed08664dade023afebd7e277e12d6ce0a4
siemiatj <kuba@saucelabs.com>
2016-04-26 20:27:09 -0700

    - work on dumping grunt in favor of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,60 @@
-'use strict';
+const path              = require('path');
+const webpack           = require('webpack');
+const cwd = process.cwd();
 
-var webpack = require('webpack');
-var path = require('path');
-
-module.exports = {
+const config = {
+  context: __dirname + "/app",
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    `./app.js`
+  ],
   output: {
-    path: 'dist/',
-    filename: '[name].js'
-  },
-  entry: {
-    'js/app': './app/js/app.bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   resolve: {
-    modulesDirectories: ['node_modules']
+    modulesDirectories: [cwd, 'node_modules'],
   },
-  cache: true,
-
-  stats: {
-    colors: true,
-    reasons: true
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
   },
+  devtool: 'eval-source-map',
+  noInfo: true,
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: [__dirname],
+        exclude: [path.join(__dirname, './', 'node_modules')]
+      }
+    ],
     loaders: [
-      { test: /\.es6$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' }
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style!css!autoprefixer?browsers=last 2 versions'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+      }
     ]
-  }
+  },
+  plugins: []
 };
+
+module.exports = config;
