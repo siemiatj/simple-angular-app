f8998e19edd3c92cf9b88571bf73644c0b8b4b09
siemiatj <kuba@saucelabs.com>
2016-05-02 02:11:03 -0700

    - cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,6 @@ const config = {
   devtool: 'eval-source-map',
   noInfo: true,
   module: {
-    // preLoaders: [
-    //   {
-    //     test: /\.js$/,
-    //     loader: 'eslint',
-    //     include: [__dirname],
-    //     exclude: [path.join(__dirname, './', 'node_modules')]
-    //   }
-    // ],
     loaders: [
       {
         test: /\.es6?$/,
@@ -38,37 +30,13 @@ const config = {
       },
       {
         test: /\.scss$/,
-        // exclude: /node_modules/,
         loaders: ['style', 'css', 'sass']
       },
-      // {
-      //   test: /\.css$/,
-      //   exclude: /node_modules/,
-      //   loader: 'style!css!autoprefixer?browsers=last 2 versions'
-      // },
-      // {
-      //   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      //   exclude: [/node_modules/, /assets/],
-      //   loader: 'file-loader'
-      // },
-      // {
-      //   test: /\.svg|png|jpg$/,
-      //   loader: 'file?name=images/[name].[ext]'
-      // },
-      // {
-      //   test: /\.(jpg|png)$/,
-      //   loader: 'file?name=assets/[name].[ext]',
-      //   include: PATHS.assets
-      // },
       {
         test: /\.svg$/,
         loader: 'file',
         include: PATHS.assets
       },
-      // { 
-      //   test: /\.(png|jpg)$/, 
-      //   loader: 'url-loader?limit=8192' 
-      // },
       {
         test: /\.css$/,
         loader: 'style!css'
@@ -77,10 +45,6 @@ const config = {
         test: /\.html$/,
         loader: 'ngtemplate?relativeTo=' + PATHS.app + '/!html'
       },
-        // {
-        //     test: /\.scss$/,
-        //     loader: "style!css!sass"
-        // }, 
       {
         test: [/fontawesome-webfont\.svg/, /fontawesome-webfont\.eot/, /fontawesome-webfont\.ttf/, /fontawesome-webfont\.woff/, /fontawesome-webfont\.woff2/],
         loader: 'file?name=fonts/[name].[ext]',
